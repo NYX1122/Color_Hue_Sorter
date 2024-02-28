615a4e7cf319769e667e55e110304c6d4a37f4d4
BoDee Angus <bodee.angus@gmail.com>
2024-02-27 23:50:27 -0700

    Finished

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-export default function app(colors) {}
+import chroma from 'chroma-js';
+
+import colorDisplayer from 'color_displayer';
+
+export default function app(colors) {
+  const sortedColors = colors
+    .map((color) => ({
+      color,
+      hcl: chroma(color).hcl(),
+    }))
+    .sort((a, b) => a.hcl[0] - b.hcl[0])
+    .map((sortedColor) => chroma(sortedColor.color).hex());
+
+  console.log('Sorted colors:');
+  colorDisplayer(sortedColors);
+  return sortedColors;
+}
